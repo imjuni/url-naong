7f10546041623c0637544f7d2f76fe725fa2b236
ByungJoon Lee <imjuni@gmail.com>
2025-08-23 01:17:33 +0900

    feat(url): params support undefined and null
    - enhance: params support undefined and null
    - migrate: eslint v8 > v9
    - deps
      - dev
        - add @eslint/compat: 1.3.2
        - add @eslint/js: 9.33.0
        - add @stylistic/eslint-plugin: 3.1.0
        - add @tsconfig/node20: 20.1.6
        - add eslint-config-airbnb-extended: 2.1.2
        - add eslint-plugin-import-x: 4.16.1
        - add eslint-plugin-n: 17.21.3
        - add typescript-eslint: 8.40.0
        - change @types/node: 20.13.0 > 20.19.11
        - change @vitest/coverage-v8: 1.6.0 > 3.2.4
        - change eslint: 8.57.0 > 9.33.0
        - change eslint-config-prettier: 9.1.0 > 10.1.8
        - change eslint-import-resolver-typescript: 3.5.5 > 4.4.4
        - change eslint-plugin-prettier: 5.0.0 > 5.5.4
        - change prettier: 3.0.0 > 3.2.5
        - change typescript: 5.4.5 > 5.9.2
        - change vite-tsconfig-paths: 4.3.2 > 5.1.4
        - change vitest: 1.6.0 > 3.2.4
        - remove @tsconfig/node18: 18.2.4
        - remove @typescript-eslint/eslint-plugin: 7.11.0
        - remove @typescript-eslint/parser: 7.11.0
        - remove eslint-config-airbnb-typescript: 18.0.0
        - remove eslint-plugin-import: 2.27.5
        - remove eslint-plugin-jsdoc: 48.2.7
        - remove eslint-plugin-prefer-arrow: 1.2.3
        - remove prettier-eslint: 16.3.0
        - remove prettier-plugin-organize-imports: 3.2.3

--- src/urlna.test.ts
+++ src/urlna.test.ts
@@ -1,5 +1,5 @@
 import { describe, expect, it } from 'vitest';
-import { urlna } from '..';
+import { urlna } from '.';
 
 describe('urlna', () => {
   it('join function skip start slash removing', () => {
@@ -7,6 +7,7 @@ describe('urlna', () => {
       name: 'ironman',
       hid: 1,
       cid: 13,
+      age: undefined,
       tid: '72368bb0-a6cc-4fa1-9288-c903fd45c333',
     });
     expect(url).toEqual('/hero/1/comic/13/name/ironman?tid=72368bb0-a6cc-4fa1-9288-c903fd45c333');
@@ -40,6 +41,28 @@ describe('urlna', () => {
       tid: '72368bb0-a6cc-4fa1-9288-c903fd45c333',
     });
     expect(url).toEqual('http://localhost/hero/1/comic/13/name/ironman?tid=72368bb0-a6cc-4fa1-9288-c903fd45c333');
+  });
+
+  it('array.parameter.test', () => {
+    const url = urlna(
+      'http://localhost',
+      '/hero/:hid/comic/:cid/name/:name',
+      {
+        name: 'ironman',
+        hid: 1,
+        cid: 13,
+        tid: '72368bb0-a6cc-4fa1-9288-c903fd45c333',
+        teamIds: [1, 2, 3, 4],
+        category: ['marvel', 'advengers', 'S.H.I.E.L.D'],
+      },
+      {
+        arrayFormat: 'repeat',
+      },
+    );
+
+    expect(url).toEqual(
+      'http://localhost/hero/1/comic/13/name/ironman?tid=72368bb0-a6cc-4fa1-9288-c903fd45c333&teamIds=1&teamIds=2&teamIds=3&teamIds=4&category=marvel&category=advengers&category=S.H.I.E.L.D',
+    );
   });
 
   it('array.parameter.test', () => {
